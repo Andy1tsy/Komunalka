ec0ca07233f81bf45a70d32ed2baaf350879a0d1
Andrii Tsyhankov <andy1tsy@gmail.com>
2020-08-14 22:35:44 +0300

    Added base of business logic layer

---
+++ Komunalka.BLL/DTO/CustomerDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Komunalka.API.DTO
+{
+    public class CustomerDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public  List<PaymentDTO> PaymentsDTO { get; set; }
+        public CustomerDTO()
+        {
+            PaymentsDTO = new List<PaymentDTO>();
+        }
+    }
+}
---
+++ Komunalka.BLL/DTO/PayingFixedSummaDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Komunalka.API.DTO
+{
+    public class PayingFixedSummaDTO
+    {
+        public string ServiceProviderNane { get; set; }
+        public string Account { get; set; }
+        public decimal Summa { get; set; }
+
+        public PayingFixedSummaDTO()
+        {
+
+        }
+    }
+}
