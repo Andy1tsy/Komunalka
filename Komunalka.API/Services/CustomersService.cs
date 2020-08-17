7558990ee8be76ff477353c2fb10aa79c88b61d3
Andrii Tsyhankov <andy1tsy@gmail.com>
2020-08-17 21:27:27 +0300

    Some more refactoring

---
+++ Komunalka.API/Services/CustomersService.cs
@@ -0,0 +1,21 @@
+﻿using Komunalka.API.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Komunalka.BLL.Services
+{
+    public class CustomersService
+    {
+
+        public CustomersService()
+        {
+        }
+
+        public async IEnumerable<CustomerDTO> GetCustomersAsync()
+        {
+
+        }
+
+    }
+}
