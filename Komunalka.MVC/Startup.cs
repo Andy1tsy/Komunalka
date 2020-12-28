3183024242ae9bc973b41aa12ef017f5acbbb666
Andrii Tsyhankov <andy1tsy@gmail.com>
2020-12-28 10:34:57 +0200

    Update Startup.cs

--- Komunalka.MVC/Startup.cs
+++ Komunalka.MVC/Startup.cs
@@ -72,7 +72,7 @@ namespace Komunalka.MVC
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=CustomersMVC}/{action=Index}/{id?}");
             });
         }
     }
